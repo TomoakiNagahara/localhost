76f543c05282d6239ff7d0f367b89412e4f8ed15
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2022-12-25 14:44:46 +0900

    New: switcher.phtml

--- index.php
+++ index.php
@@ -28,29 +28,8 @@ error_reporting(E_ALL);
 
 	<!-- content -->
 	<section class="content">
-		<!-- title -->
-		<h1>index.php</h1>
-
-		<!-- link -->
-		<div class="block">
-			<a href="index.html">index.html</a>
-		</div>
-
-		<!-- message area -->
-		<div>
-			<span id="message" class=""></span>
-		</div>
-
-		<!-- version -->
-		<form class="block">
-			switch php version:
-			<select id="switch_php_version">
-				<option></option>
-				<?php foreach(array(53,54,55,56,70,71,72,73,74,80,81) as $version): ?>
-				<option><?php echo $version ?></option>
-				<?php endforeach; ?>
-			</select>
-		</form>
+		<!-- PHP version switcher -->
+		<?php include('switcher.phtml'); ?>
 
 		<!-- $_SERVER -->
 		<table>
